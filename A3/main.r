9edc1963349dc095925a26ec23f07c234ff8a0e1
anaramirli <anar.amirli@gmail.com>
2020-12-16 23:27:25 +0100

    Update main.r

--- A3/main.r
+++ A3/main.r
@@ -59,17 +59,76 @@ mean((test$lpsa - predict(glm.fit, test))^2)
 
 # TASK 3
 
-x = model.matrix(lpsa???.,train)[,-1]
-y = train$lpsa;
+# get train/test matrices
+x.train = model.matrix(lpsa???.,train)[,-1]
+y.train = train$lpsa;
+
+x.test = model.matrix(lpsa???.,test)[,-1]
+y.test = test$lpsa;
 
 
 # create sequence of values for lmabda ranging from 10^-2 to 10^10 
 lmabda =10^seq(-2, 10, length = 100)
 # we've already standardize data, thus we set standardize=FALSE
-ridge.mod=glmnet (x, y, alpha=0, lambda=lmabda, standardize=FALSE) 
+ridge.mod=glmnet (x.train, y.train, alpha=0, lambda=lmabda, standardize=FALSE) 
 
 
 plot(ridge.mod, xvar = "lambda", label = TRUE)
 
 
-# TAKS 4
+# TASK 4
+
+m.ridge.cv <- cv.glmnet(x.train, y.train, alpha = 0, lambda = lmabda, nfolds=10, standardize=FALSE)
+plot(m.ridge.cv)
+
+# best lmabda value
+bestlam.ridge <- m.ridge.cv$lambda.min
+bestlam.ridge
+
+
+# # prediction for the training/test sets
+m.ridge.cv.pred.train = predict(m.ridge.cv, newx = x.train, s = bestlam.ridge)
+m.ridge.cv.pred.test  = predict(m.ridge.cv, newx = x.test, s = bestlam.ridge)
+
+# calculate rmse on training/test sets
+print(paste('RMSE on training set:', mean((m.ridge.cv.pred.train - y.train)^2)))
+print(paste('RMSE on test set:', mean((m.ridge.cv.pred.test - y.test)^2)))
+
+ridge.coef <- predict(m.ridge.cv, s = bestlam.ridge, type = "coefficients")
+ridge.coef
+# gleason is the non-used feature, where its coefficent equals to zero.
+
+
+
+# TASK 5
+
+lasso.mod <- glmnet (x.train, y.train, alpha=1, lambda=lmabda, standardize=FALSE) 
+plot(lasso.mod, xvar = "lambda", label = TRUE)
+
+
+# TASK 6
+
+m.lasso.cv <- cv.glmnet(x.train, y.train, alpha = 1, lambda = lmabda, nfolds=10, standardize=FALSE)
+plot(m.lasso.cv)
+
+# best lmabda value
+bestlam.lasso <- m.lasso.cv$lambda.min
+bestlam.lasso
+
+
+
+
+# # prediction for the training/test sets
+m.lasso.cv.pred.train <- predict(m.lasso.cv, newx = x.train, s = bestlam.lasso)
+m.lasso.cv.pred.test  <- predict(m.lasso.cv, newx = x.test, s = bestlam.lasso)
+
+# calculate rmse on training/test sets
+print(paste('RMSE on training set:', mean((m.lasso.cv.pred.train - y.train)^2)))
+print(paste('RMSE on test set:', mean((m.lasso.cv.pred.test - y.test)^2)))
+
+feature_names
+
+lasso.coef <- predict(m.lasso.cv, s = bestlam.lasso, type = "coefficients")
+lasso.coef
+# gleason is the non-used feature, where its coefficent equals to zero.
+
