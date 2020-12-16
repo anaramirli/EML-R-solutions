b63000c12ad173b5c1bafcf3dcc2a355c5544197
anaramirli <anar.amirli@gmail.com>
2020-12-16 21:39:08 +0100

    Update main.r

--- A3/main.r
+++ A3/main.r
@@ -12,11 +12,9 @@ df<-read.table("prostate.txt")
 train <-subset(df, train == "TRUE", select=-(train))
 test <-subset(df, train == "FALSE", select=-(train))
 
-# normalize each input feature to a mean of 0 and a variance of 1
-# exclude the target from features
-
+# exclude the target from the features
 feature_names <- colnames(subset(train, select=-(lpsa)))
-
+# normalize each input feature to a mean of 0 and a variance of 1
 for (i_name in feature_names){
   
   # get the mean and var of train features
@@ -39,14 +37,20 @@ glm.fit = glm(formula ,data=train)
 # LOOCV
 loocv.err = cv.glm(train , glm.fit)
 loocv.err$delta[1]
-
+# [1] 0.5839552
 
 # 5-fold CV
-cv.5.err = cv.glm(train , glm.fit, K=5)
+cv.5.err = cv.glm(train , glm.fit, K=5) # the default cost fun is the average squared error function
 cv.5.err$delta[1]
+# 0.6833892
 
 
 # 10-fold CV
 cv.10.err = cv.glm(train , glm.fit, K=10)
 cv.10.err$delta[1]
+# 0.602335
 
+
+# predict test
+mean((test$lpsa - predict(glm.fit, test))^2)
+# [1] 0.521274
