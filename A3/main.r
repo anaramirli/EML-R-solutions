3f4dbe5619bdb71669ee362757650c7ed5b099e3
anaramirli <anar.amirli@gmail.com>
2020-12-16 21:23:25 +0100

    Update main.r

--- A3/main.r
+++ A3/main.r
@@ -27,11 +27,27 @@ for (col_name in colnames(subset(train, select=-(lpsa)))){
 # TASK 2
 
 
+# LOOCV
+
+library(boot)
+
+glm.fit=glm(mpg???horsepower ,data=Auto)
+> cv.err=cv.glm(Auto ,glm.fit)
+> cv.err$delta
+
+
+cv.error.5=rep(0,5)
+for (i in 1:5){
+  glm.fit=glm(mpg~horsepower ,i),data=Auto)
+  cv.error[i]=cv.glm(Auto ,glm.fit)$delta [1]
+}
+
 > cv.error=rep(0,5)
 > for (i in 1:5){
-  + glm.fit=glm(mpg∼poly(horsepower ,i),data=Auto)
+  + glm.fit=glm(mpg???poly(horsepower ,i),data=Auto)
   + cv.error[i]=cv.glm(Auto ,glm.fit)$delta [1]
   + }
+> cv.error
   
 
 
